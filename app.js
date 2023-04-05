8d277bc96b9532a00b80f57acc381e13be4223ca
Simul <simulbista@gmail.com>
2023-04-05 19:31:45 -0400

    changed the logic for all the routes, moved the logic from hbs to the function in app.js

--- app.js
+++ app.js
@@ -7,24 +7,28 @@ Name: ________Simul Bista______Student ID: _ N01489966____ Date: ______April 3, 
 const express = require("express");
 const app = express();
 const port = 3000;
-const handlebars = require("express-handlebars").engine;
+const handlebars = require("express-handlebars");
 const fs = require("fs");
 
-
 //register eq helper method to be used in data.hbs to compare if searched title from the form exists in books.json
-// handlebars.create({ helpers: { eq: (a, b) => a === b } });
+const hbs = handlebars.create({
+  layoutsDir: `${__dirname}/views/layouts`,
+  //reduce handlebars extension name to hbs
+  extname: "hbs",
+  defaultLayout: "index",
+  partialsDir: `${__dirname}/views/partials`,
+
+  // create custom helpers
+  helpers: {
+    eq: function (a, b) {
+      console.log("a:", a, "b:", b);
+      return a === b;
+    },
+  },
+});
 
 app.set("view engine", "hbs");
-app.engine(
-  '.hbs',
-  handlebars({
-    layoutsDir: `${__dirname}/views/layouts`,
-    //reduce handlebars extension name to hbs
-    extname: "hbs",
-    defaultLayout: "index",
-    partialsDir: `${__dirname}/views/partials`,
-  })
-);
+app.engine("hbs", hbs.engine);
 
 // Serve static files from the public directory
 app.use(express.static("public"));
@@ -40,26 +44,53 @@ app.get("/data", (req, res) => {
   res.render("data", { books });
 });
 
-//display isbn for books based on the index number in the url
-// http://localhost:5500/data/isbn/0 should display 1933988673
-app.get("/data/isbn/:iNo", (req, res) => {
+app.get("/data/isbn/:isbn", (req, res) => {
   const myData = fs.readFileSync("books.json");
   const books = JSON.parse(myData);
-  iNo = req.params.iNo;
-  res.render("data-isbn",{'searchedINo':iNo,'foundIsbn':books[iNo].isbn});
+  let foundBook = null;
+  searchedIsbn = req.params.isbn;
+
+  for (let id = 0; id < books.length; id++) {
+    if (books[id].isbn === searchedIsbn) {
+      foundBook = books[id];
+      break;
+    }
+  }
+
+  if (foundBook) {
+    res.render("data-isbn", { foundBook: foundBook, searchedIsbnNo: searchedIsbn });
+  } else {
+    res.render("data-isbn", { searchedIsbnNo: searchedIsbn });
+  }
+
 });
+
 
 /** Decode Form URL Encoded data */
 app.use(express.urlencoded({ extended: true }));
 
 // Process POST request through the form for title search
 app.post("/submittedTitle", function (req, res, next) {
-  let myData = fs.readFileSync("books.json");
-  let books = JSON.parse(myData);
-  const { title } = req.body;
-  res.render("data",{'books':books,'searchedTitle':title});
+  // console.log('im in app.js');
+  const myData = fs.readFileSync("books.json");
+  const books = JSON.parse(myData);
+  let foundBook,notfoundBook = null;
+  submittedTitle = req.body.title;
+  for (let id = 0; id < books.length; id++) {
+    if (books[id].title === submittedTitle) {
+      foundBook = books[id];
+      break;
+    }
+  }
 
+  if (foundBook) {
+    console.log(foundBook);
+    res.render("data", { foundBook: foundBook, submittedTitle: submittedTitle });
+  } else {
+    res.render("data", { notfoundBook: notfoundBook,submittedTitle: submittedTitle });
+  }
 });
+
 
 app.listen(port, () => {
   console.log(`App listening to port ${port}!`);
